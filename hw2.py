676f83a3d2e4515fb2662c968b0637284abf2351
oscar9335 <oscar9335@gmail.com>
2023-09-27 12:48:24 +0800

    hw2_1 first commit

---
+++ hw2.py
@@ -0,0 +1,14 @@
+import random
+import math
+from math import e
+
+def simulation_one(x,cast_time):
+    function_one = (x+x**x)
+    e**function_one
+
+    num = random(-2,2)
+
+
+num = random.uniform(-2,2)
+
+print(num)
