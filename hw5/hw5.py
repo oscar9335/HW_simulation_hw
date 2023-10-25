4dc155b761cd18093328d8d9755793abe5121d26
oscar9335 <oscar9335@gmail.com>
2023-10-25 17:01:36 +0800

    10% progress

---
+++ hw5/hw5.py
@@ -0,0 +1,36 @@
+import math
+import random
+import numpy as np
+import matplotlib.pyplot as plt  
+
+def gen_Y_expo():
+    return -math.log(random.uniform(0,1))
+
+def generate_first():
+    while(True):
+        U = random.uniform(0,1)
+        Y = gen_Y_expo()
+        if U <= math.e**((-0.5*((Y-1)**2))):
+            if random.random() > 0.5:
+                X = Y
+            else:
+                X = -Y
+            break
+
+    return X
+
+all_X = []
+
+for i in range(100000):
+    all_X.append(generate_first())
+
+# print(all_X)
+
+
+
+# plt.figure(figsize = (20, 10))
+p1 = plt.hist(all_X,bins='auto',density=True)
+plt.xlabel("X")
+plt.ylabel("how many times it appeard")
+plt.title("")
+plt.show()
